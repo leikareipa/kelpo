48ff0d39f827ab7ebb3b6ce3dab27f1c1cdb9939
leikareipa <drilledicecream@gmail.com>
2019-09-21 11:13:06 +0300

    Clean up the build scripts a bit

--- build_render_opengl_win32.sh
+++ build_render_opengl_win32.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# USE MINGW/WINE TO BUILD RGEO FOR WIN32, WITH 3D RENDERING VIA LEGACY OPENGL.
+# Build shiet's OpenGL renderer for Win32.
 
 GCC_PATH=~/compi/mingw471
-
-EXE_NAME="shiet_render_opengl.dll"
-BUILD_OPTIONS="-shared -nostdinc -s -ansi -pedantic -Wall -march=pentium -I./src/ -I$GCC_PATH/lib/gcc/mingw32/4.7.1/include -I$GCC_PATH/lib/gcc/mingw32/4.7.1/include/c++/mingw32/ -I$GCC_PATH/lib/gcc/mingw32/4.7.1/include/c++ -I$GCC_PATH/include"
+OUTPUT_FILE_NAME="shiet_render_opengl.dll"
+BUILD_OPTIONS="-shared -nostdinc -s -ansi -pedantic -Wall -march=pentium -I./src/ -I$GCC_PATH/lib/gcc/mingw32/4.7.1/include -I$GCC_PATH/include"
 
 SRC_FILES="
 src\\shiet_lib\\render\\render_opengl.c
@@ -14,12 +13,11 @@ src\\shiet_lib\\render\\rasterizer\\opengl\\surface_opengl.c
 src\\shiet_lib\\render\\window\\win32\\window_win32.c
 "
 
-echo "Building OpenGL..."
-wine "$GCC_PATH/bin/gcc.exe" $BUILD_OPTIONS -o $EXE_NAME $SRC_FILES -lm -lgdi32 -lwinmm -lopengl32 -lglu32
+wine "$GCC_PATH/bin/gcc.exe" $BUILD_OPTIONS -o $OUTPUT_FILE_NAME $SRC_FILES -lm -lgdi32 -lwinmm -lopengl32 -lglu32
 
-if [ -e $EXE_NAME ]
+if [ -e $OUTPUT_FILE_NAME ]
 then
-	mv $EXE_NAME bin/$EXE_NAME
+	mv $OUTPUT_FILE_NAME bin/$OUTPUT_FILE_NAME
 else
 	echo Build failed.
 	exit 1
--- build_win32.sh
+++ build_win32.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-GCC_PATH=~/compi/mingw471
+# Build shiet for Win32.
 
-EXE_NAME="shiet.exe"
-BUILD_OPTIONS="-nostdinc -s -ansi -pedantic -Wall -march=pentium -I./src/ -I$GCC_PATH/lib/gcc/mingw32/4.7.1/include -I$GCC_PATH/lib/gcc/mingw32/4.7.1/include/c++/mingw32/ -I$GCC_PATH/lib/gcc/mingw32/4.7.1/include/c++ -I$GCC_PATH/include"
+GCC_PATH=~/compi/mingw471
+OUTPUT_FILE_NAME="shiet.exe"
+BUILD_OPTIONS="-nostdinc -s -ansi -pedantic -Wall -march=pentium -I./src/ -I$GCC_PATH/lib/gcc/mingw32/4.7.1/include -I$GCC_PATH/include"
 
 SRC_FILES="
 src\\main.c
 src\\shiet\\interface.c
 "
 
-echo "Building OpenGL..."
-wine "$GCC_PATH/bin/gcc.exe" $BUILD_OPTIONS -o $EXE_NAME $SRC_FILES -lm -lgdi32 -lwinmm
+wine "$GCC_PATH/bin/gcc.exe" $BUILD_OPTIONS -o $OUTPUT_FILE_NAME $SRC_FILES -lm
 
-if [ -e $EXE_NAME ]
+if [ -e $OUTPUT_FILE_NAME ]
 then
-	mv $EXE_NAME bin/$EXE_NAME
+	mv $OUTPUT_FILE_NAME bin/$OUTPUT_FILE_NAME
 else
 	echo Build failed.
 	exit 1

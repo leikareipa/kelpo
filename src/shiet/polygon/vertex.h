65d43823b924af236aadf8fd9d41317f1a4fb77a
leikareipa <drilledicecream@gmail.com>
2019-09-22 04:06:26 +0300

    Vertex: Use float instead of int (fixedpoint)

--- src/shiet/polygon/vertex.h
+++ src/shiet/polygon/vertex.h
@@ -1,12 +1,13 @@
 #ifndef SHIET_POLYGON_VERTEX_H_
 #define SHIET_POLYGON_VERTEX_H_
 
-#include "shiet/common/types.h"
-
 struct shiet_polygon_vertex_s
 {
-    shiet_fixedpoint_t x, y, z, w;
-    real u, v; /// TODO: Use fixed-point.
+    /* World coordinates.*/
+    float x, y, z, w;
+
+    /* Texture coordinates.*/
+    float u, v;
 };
 
 #endif

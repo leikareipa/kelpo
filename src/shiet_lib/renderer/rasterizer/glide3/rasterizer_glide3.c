dc0b5efcb95db9bcdfd90b63e87b138bdd55b65c
leikareipa <drilledicecream@gmail.com>
2020-01-02 20:18:02 +0200

    Replace tabs with spaces in Glide 3.x renderer's source
    For consistency with the rest of the codebase.

--- src/shiet_lib/renderer/rasterizer/glide3/rasterizer_glide3.c
+++ src/shiet_lib/renderer/rasterizer/glide3/rasterizer_glide3.c
@@ -27,193 +27,193 @@ static FxU32 CURRENT_TEXTURE_ADDRESS = 0;
  * their order we give via grVertexLayout() in shiet_rasterizer_glide3__initialize().*/
 struct glide_vert_s
 {
-	FxFloat x, y;
-	FxFloat w;
-	FxFloat r, g, b;
-	FxFloat s, t;
+    FxFloat x, y;
+    FxFloat w;
+    FxFloat r, g, b;
+    FxFloat s, t;
 };
 
 void shiet_rasterizer_glide3__initialize(void)
 {
-	grColorMask(FXTRUE, FXFALSE);
+    grColorMask(FXTRUE, FXFALSE);
 
-	/* Vertex data layout. Matches with the glide_vert_s struct.*/
-	grVertexLayout(GR_PARAM_XY, 0, GR_PARAM_ENABLE);
-	grVertexLayout(GR_PARAM_Q, 8, GR_PARAM_ENABLE); 
-	grVertexLayout(GR_PARAM_RGB, 12, GR_PARAM_ENABLE);
-	grVertexLayout(GR_PARAM_ST0, 24, GR_PARAM_ENABLE);
+    /* Vertex data layout. Matches with the glide_vert_s struct.*/
+    grVertexLayout(GR_PARAM_XY, 0, GR_PARAM_ENABLE);
+    grVertexLayout(GR_PARAM_Q, 8, GR_PARAM_ENABLE); 
+    grVertexLayout(GR_PARAM_RGB, 12, GR_PARAM_ENABLE);
+    grVertexLayout(GR_PARAM_ST0, 24, GR_PARAM_ENABLE);
 
-	/* Depth testing.*/
-	grDepthBufferMode(GR_DEPTHBUFFER_WBUFFER);
-	grDepthBufferFunction(GR_CMP_LESS); 
-	grDepthMask(FXTRUE);
+    /* Depth testing.*/
+    grDepthBufferMode(GR_DEPTHBUFFER_WBUFFER);
+    grDepthBufferFunction(GR_CMP_LESS); 
+    grDepthMask(FXTRUE);
 
-	/* Texturing.*/
-	grTexClampMode(GR_TMU0,
-	               GR_TEXTURECLAMP_CLAMP,
-				   GR_TEXTURECLAMP_CLAMP);
-	grTexCombine(GR_TMU0,
-	             GR_COMBINE_FUNCTION_LOCAL,
-				 GR_COMBINE_FACTOR_NONE,
-				 GR_COMBINE_FUNCTION_LOCAL,
-				 GR_COMBINE_FACTOR_NONE,
-				 FXFALSE,
-				 FXFALSE); /* Set up for decal texturing.*/
-	grTexMipMapMode(GR_TMU0, GR_MIPMAP_DISABLE, FXFALSE);
+    /* Texturing.*/
+    grTexClampMode(GR_TMU0,
+                   GR_TEXTURECLAMP_CLAMP,
+                   GR_TEXTURECLAMP_CLAMP);
+    grTexCombine(GR_TMU0,
+                 GR_COMBINE_FUNCTION_LOCAL,
+                 GR_COMBINE_FACTOR_NONE,
+                 GR_COMBINE_FUNCTION_LOCAL,
+                 GR_COMBINE_FACTOR_NONE,
+                 FXFALSE,
+                 FXFALSE); /* Set up for decal texturing.*/
+    grTexMipMapMode(GR_TMU0, GR_MIPMAP_DISABLE, FXFALSE);
 
-	CURRENT_TEXTURE_ADDRESS = grTexMinAddress(GR_TMU0);
+    CURRENT_TEXTURE_ADDRESS = grTexMinAddress(GR_TMU0);
 
-	return;
+    return;
 }
 
 void shiet_rasterizer_glide3__clear_frame(void)
 {
-	grBufferClear(0, 0, ~0u);
+    grBufferClear(0, 0, ~0u);
 
-	return;
+    return;
 }
 
 static GrTexInfo generate_glide_texture_info(const struct shiet_polygon_texture_s *const texture)
 {
-	GrTexInfo info;
+    GrTexInfo info;
 
-	assert(!texture &&
-	       "Glide 3.x renderer: Received a NULL texture.");
+    assert(!texture &&
+           "Glide 3.x renderer: Received a NULL texture.");
 
-	assert((texture->width == texture->height) &&
-	       "Glide 3.x renderer: The given texture is not square.");
+    assert((texture->width == texture->height) &&
+           "Glide 3.x renderer: The given texture is not square.");
 
-	assert((texture->width >= MIN_TEXTURE_SIZE) &&
-	       (texture->width <= MAX_TEXTURE_SIZE) &&
-	       "Glide 3.x renderer: The given texture's dimensions are out of range.");
+    assert((texture->width >= MIN_TEXTURE_SIZE) &&
+           (texture->width <= MAX_TEXTURE_SIZE) &&
+           "Glide 3.x renderer: The given texture's dimensions are out of range.");
 
-	switch (texture->width)
-	{
-		case 2: info.smallLodLog2 = info.largeLodLog2 = GR_LOD_LOG2_2; break;
-		case 4: info.smallLodLog2 = info.largeLodLog2 = GR_LOD_LOG2_4; break;
-		case 8: info.smallLodLog2 = info.largeLodLog2 = GR_LOD_LOG2_8; break;
-		case 16: info.smallLodLog2 = info.largeLodLog2 = GR_LOD_LOG2_16; break;
-		case 32: info.smallLodLog2 = info.largeLodLog2 = GR_LOD_LOG2_32; break;
-		case 64: info.smallLodLog2 = info.largeLodLog2 = GR_LOD_LOG2_64; break;
-		case 128: info.smallLodLog2 = info.largeLodLog2 = GR_LOD_LOG2_128; break;
-		case 256: info.smallLodLog2 = info.largeLodLog2 = GR_LOD_LOG2_256; break;
-	}
+    switch (texture->width)
+    {
+        case 2: info.smallLodLog2 = info.largeLodLog2 = GR_LOD_LOG2_2; break;
+        case 4: info.smallLodLog2 = info.largeLodLog2 = GR_LOD_LOG2_4; break;
+        case 8: info.smallLodLog2 = info.largeLodLog2 = GR_LOD_LOG2_8; break;
+        case 16: info.smallLodLog2 = info.largeLodLog2 = GR_LOD_LOG2_16; break;
+        case 32: info.smallLodLog2 = info.largeLodLog2 = GR_LOD_LOG2_32; break;
+        case 64: info.smallLodLog2 = info.largeLodLog2 = GR_LOD_LOG2_64; break;
+        case 128: info.smallLodLog2 = info.largeLodLog2 = GR_LOD_LOG2_128; break;
+        case 256: info.smallLodLog2 = info.largeLodLog2 = GR_LOD_LOG2_256; break;
+    }
 
-	info.aspectRatioLog2 = GR_ASPECT_LOG2_1x1;
-	info.format = GR_TEXFMT_ARGB_1555;
-	info.data = (FxU16*)texture->pixelArray;
+    info.aspectRatioLog2 = GR_ASPECT_LOG2_1x1;
+    info.format = GR_TEXFMT_ARGB_1555;
+    info.data = (FxU16*)texture->pixelArray;
 
-	return info;
+    return info;
 }
 
 void shiet_rasterizer_glide3__upload_texture(struct shiet_polygon_texture_s *const texture)
 {
-	GrTexInfo texInfo;
-	FxU32 textureSize; 
+    GrTexInfo texInfo;
+    FxU32 textureSize; 
 
-	if (!texture)
+    if (!texture)
     {
         return;
     }
 
-	texInfo = generate_glide_texture_info(texture);
-	textureSize = grTexTextureMemRequired(GR_MIPMAPLEVELMASK_BOTH, &texInfo); 
+    texInfo = generate_glide_texture_info(texture);
+    textureSize = grTexTextureMemRequired(GR_MIPMAPLEVELMASK_BOTH, &texInfo); 
 
-	assert((CURRENT_TEXTURE_ADDRESS + textureSize) > grTexMaxAddress(GR_TMU0) &&
-	       "Glide 3.x renderer: Not enough texture memory to store the given texture.");
+    assert((CURRENT_TEXTURE_ADDRESS + textureSize) > grTexMaxAddress(GR_TMU0) &&
+           "Glide 3.x renderer: Not enough texture memory to store the given texture.");
 
-	grTexDownloadMipMap(GR_TMU0, CURRENT_TEXTURE_ADDRESS, GR_MIPMAPLEVELMASK_BOTH, &texInfo);
-	texture->apiId = CURRENT_TEXTURE_ADDRESS;
+    grTexDownloadMipMap(GR_TMU0, CURRENT_TEXTURE_ADDRESS, GR_MIPMAPLEVELMASK_BOTH, &texInfo);
+    texture->apiId = CURRENT_TEXTURE_ADDRESS;
 
-	CURRENT_TEXTURE_ADDRESS += textureSize;
+    CURRENT_TEXTURE_ADDRESS += textureSize;
 
-	return;
+    return;
 }
 
 void shiet_rasterizer_glide3__update_texture(const struct shiet_polygon_texture_s *const texture)
 {
-	GrTexInfo texInfo;
-	
-	if (!texture)
+    GrTexInfo texInfo;
+    
+    if (!texture)
     {
         return;
     }
 
-	texInfo = generate_glide_texture_info(texture);
+    texInfo = generate_glide_texture_info(texture);
 
-	grTexDownloadMipMap(GR_TMU0, texture->apiId, GR_MIPMAPLEVELMASK_BOTH, &texInfo);
+    grTexDownloadMipMap(GR_TMU0, texture->apiId, GR_MIPMAPLEVELMASK_BOTH, &texInfo);
 
-	return;
+    return;
 }
 
 
 void shiet_rasterizer_glide3__draw_triangles(const struct shiet_polygon_triangle_s *const triangles,
                                              const unsigned numTriangles)
 {
-	unsigned i = 0, v = 0;
+    unsigned i = 0, v = 0;
 
     for (i = 0; i < numTriangles; i++)
     {
-		struct glide_vert_s glideVertex[3];
-		const struct shiet_polygon_texture_s *const texture = triangles[i].material.texture;
+        struct glide_vert_s glideVertex[3];
+        const struct shiet_polygon_texture_s *const texture = triangles[i].material.texture;
 
-		for (v = 0; v < 3; v++)
-		{
-			glideVertex[v].w = (1 / triangles[i].vertex[v].w);
+        for (v = 0; v < 3; v++)
+        {
+            glideVertex[v].w = (1 / triangles[i].vertex[v].w);
 
-			glideVertex[v].r = triangles[i].material.baseColor[0];
-			glideVertex[v].g = triangles[i].material.baseColor[1];
-			glideVertex[v].b = triangles[i].material.baseColor[2];
+            glideVertex[v].r = triangles[i].material.baseColor[0];
+            glideVertex[v].g = triangles[i].material.baseColor[1];
+            glideVertex[v].b = triangles[i].material.baseColor[2];
 
-			glideVertex[v].x = triangles[i].vertex[v].x;
-			glideVertex[v].y = triangles[i].vertex[v].y;
-		}
-	
-		/* Set the rendering mode based on whether the triangle has a texture
-		 * or is solid-filled.*/
-		if (!texture)
-		{
-			grColorCombine(GR_COMBINE_FUNCTION_LOCAL,
-			               GR_COMBINE_FACTOR_NONE,
-						   GR_COMBINE_LOCAL_ITERATED,
-						   GR_COMBINE_OTHER_NONE,
-						   FXFALSE);
-		}
-		else
-		{
-			const unsigned glideTexScale = (256 / texture->width);
-			GrTexInfo texInfo = generate_glide_texture_info(texture);
+            glideVertex[v].x = triangles[i].vertex[v].x;
+            glideVertex[v].y = triangles[i].vertex[v].y;
+        }
+    
+        /* Set the rendering mode based on whether the triangle has a texture
+         * or is solid-filled.*/
+        if (!texture)
+        {
+            grColorCombine(GR_COMBINE_FUNCTION_LOCAL,
+                           GR_COMBINE_FACTOR_NONE,
+                           GR_COMBINE_LOCAL_ITERATED,
+                           GR_COMBINE_OTHER_NONE,
+                           FXFALSE);
+        }
+        else
+        {
+            const unsigned glideTexScale = (256 / texture->width);
+            GrTexInfo texInfo = generate_glide_texture_info(texture);
 
-			for (v = 0; v < 3; v++)
-			{
-				glideVertex[v].s = ((triangles[i].vertex[0].u / triangles[i].vertex[0].w) * glideTexScale);
-				glideVertex[v].t = ((triangles[i].vertex[0].v / triangles[i].vertex[0].w) * glideTexScale);
-			}
+            for (v = 0; v < 3; v++)
+            {
+                glideVertex[v].s = ((triangles[i].vertex[0].u / triangles[i].vertex[0].w) * glideTexScale);
+                glideVertex[v].t = ((triangles[i].vertex[0].v / triangles[i].vertex[0].w) * glideTexScale);
+            }
 
-			if (texture->filtering == SHIET_TEXTURE_FILTER_LINEAR)
-			{
-				grTexFilterMode(GR_TMU0,
-				                GR_TEXTUREFILTER_BILINEAR,
-								GR_TEXTUREFILTER_BILINEAR);
-			}
-			else
-			{
-				grTexFilterMode(GR_TMU0,
-				                GR_TEXTUREFILTER_POINT_SAMPLED,
-								GR_TEXTUREFILTER_POINT_SAMPLED);
-			}
+            if (texture->filtering == SHIET_TEXTURE_FILTER_LINEAR)
+            {
+                grTexFilterMode(GR_TMU0,
+                                GR_TEXTUREFILTER_BILINEAR,
+                                GR_TEXTUREFILTER_BILINEAR);
+            }
+            else
+            {
+                grTexFilterMode(GR_TMU0,
+                                GR_TEXTUREFILTER_POINT_SAMPLED,
+                                GR_TEXTUREFILTER_POINT_SAMPLED);
+            }
 
-			grTexSource(GR_TMU0, texture->apiId, GR_MIPMAPLEVELMASK_BOTH, &texInfo);
+            grTexSource(GR_TMU0, texture->apiId, GR_MIPMAPLEVELMASK_BOTH, &texInfo);
 
-			grColorCombine(GR_COMBINE_FUNCTION_SCALE_OTHER,
-			               GR_COMBINE_FACTOR_LOCAL,
-						   GR_COMBINE_LOCAL_ITERATED,
-						   GR_COMBINE_OTHER_TEXTURE,
-						   FXFALSE);
-		}
+            grColorCombine(GR_COMBINE_FUNCTION_SCALE_OTHER,
+                           GR_COMBINE_FACTOR_LOCAL,
+                           GR_COMBINE_LOCAL_ITERATED,
+                           GR_COMBINE_OTHER_TEXTURE,
+                           FXFALSE);
+        }
 
-		grDrawTriangle(&glideVertex[0], &glideVertex[1], &glideVertex[2]);
-	}
+        grDrawTriangle(&glideVertex[0], &glideVertex[1], &glideVertex[2]);
+    }
 
-	return;
+    return;
 }
--- src/shiet_lib/renderer/rasterizer/glide3/surface_glide3_win32.c
+++ src/shiet_lib/renderer/rasterizer/glide3/surface_glide3_win32.c
@@ -20,10 +20,10 @@ static GrContext_t GLIDE_RENDER_CONTEXT = 0;
 
 void shiet_surface_glide3_win32__release_surface(void)
 {
-	assert(GLIDE_RENDER_CONTEXT &&
-	       "Glide 3.x renderer: Can't release a NULL render context.");
+    assert(GLIDE_RENDER_CONTEXT &&
+           "Glide 3.x renderer: Can't release a NULL render context.");
 
-	grSstWinClose(GLIDE_RENDER_CONTEXT);
+    grSstWinClose(GLIDE_RENDER_CONTEXT);
 
     return;
 }
@@ -40,7 +40,7 @@ void shiet_surface_glide3_win32__update_surface(void)
         DispatchMessage(&m);
     }
 
-	grBufferSwap(1);
+    grBufferSwap(1);
 
     return;
 }
@@ -51,18 +51,18 @@ void shiet_surface_glide3_win32__create_surface(const unsigned width,
 {
     int glideResolution = 0;
 
-	assert(GLIDE_RENDER_CONTEXT &&
-	       "Glide 3.x renderer: A render context already exists.");
+    assert(GLIDE_RENDER_CONTEXT &&
+           "Glide 3.x renderer: A render context already exists.");
 
-	if      ((width == 320)  && (height == 200)) glideResolution = GR_RESOLUTION_320x200;
-	else if ((width == 320)  && (height == 240)) glideResolution = GR_RESOLUTION_320x240;
-	else if ((width == 400)  && (height == 300)) glideResolution = GR_RESOLUTION_400x300;
-	else if ((width == 512)  && (height == 384)) glideResolution = GR_RESOLUTION_512x384;
-	else if ((width == 640)  && (height == 400)) glideResolution = GR_RESOLUTION_640x400;
-	else if ((width == 640)  && (height == 480)) glideResolution = GR_RESOLUTION_640x480;
-	else if ((width == 800)  && (height == 600)) glideResolution = GR_RESOLUTION_800x600;
-	else if ((width == 1024) && (height == 768)) glideResolution = GR_RESOLUTION_1024x768;
-	else assert(0 && "Glide 3.x renderer: Unsupported render resolution.");
+    if      ((width == 320)  && (height == 200)) glideResolution = GR_RESOLUTION_320x200;
+    else if ((width == 320)  && (height == 240)) glideResolution = GR_RESOLUTION_320x240;
+    else if ((width == 400)  && (height == 300)) glideResolution = GR_RESOLUTION_400x300;
+    else if ((width == 512)  && (height == 384)) glideResolution = GR_RESOLUTION_512x384;
+    else if ((width == 640)  && (height == 400)) glideResolution = GR_RESOLUTION_640x400;
+    else if ((width == 640)  && (height == 480)) glideResolution = GR_RESOLUTION_640x480;
+    else if ((width == 800)  && (height == 600)) glideResolution = GR_RESOLUTION_800x600;
+    else if ((width == 1024) && (height == 768)) glideResolution = GR_RESOLUTION_1024x768;
+    else assert(0 && "Glide 3.x renderer: Unsupported render resolution.");
 
     shiet_window_win32__create_window(width, height, windowTitle);
     shiet_window_win32__get_window_handle((void*)&WINDOW_HANDLE);
@@ -74,20 +74,20 @@ void shiet_surface_glide3_win32__create_surface(const unsigned width,
     ShowWindow(WINDOW_HANDLE, SW_SHOW);
     SetForegroundWindow(WINDOW_HANDLE);
     SetFocus(WINDOW_HANDLE);
-	UpdateWindow(WINDOW_HANDLE);
+    UpdateWindow(WINDOW_HANDLE);
 
-	grGlideInit();
-	grSstSelect(0);
-	GLIDE_RENDER_CONTEXT = grSstWinOpen((FxU32)WINDOW_HANDLE,
-	                                    glideResolution,
-										GR_REFRESH_60Hz,
-										GR_COLORFORMAT_ARGB,
-										GR_ORIGIN_UPPER_LEFT,
-										2,
-										1);
-	
-	assert(GLIDE_RENDER_CONTEXT &&
-	       "Glide 3.x renderer: Failed to initialize the renderer.");
+    grGlideInit();
+    grSstSelect(0);
+    GLIDE_RENDER_CONTEXT = grSstWinOpen((FxU32)WINDOW_HANDLE,
+                                        glideResolution,
+                                        GR_REFRESH_60Hz,
+                                        GR_COLORFORMAT_ARGB,
+                                        GR_ORIGIN_UPPER_LEFT,
+                                        2,
+                                        1);
+    
+    assert(GLIDE_RENDER_CONTEXT &&
+           "Glide 3.x renderer: Failed to initialize the renderer.");
 
     return;
 }

9416128b05606338a86af756fb9487e9f7c74c4b
leikareipa <drilledicecream@gmail.com>
2020-01-05 02:53:44 +0200

    examples/rotating triangle: Remove the texture
    Not using texturing in this basic example makes the code simpler to read.

--- examples/rotating_triangle/src/main.c
+++ examples/rotating_triangle/src/main.c
@@ -8,7 +8,6 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
 #include <shiet/polygon/triangle/triangle_stack.h>
 #include <shiet/polygon/triangle/triangle.h>
 #include <shiet/renderer_interface.h>
@@ -17,14 +16,11 @@
 
 int main(void)
 {
-    const struct { unsigned width; unsigned height; } renderResolution = {1024, 768};
+    const struct { unsigned width; unsigned height; } renderResolution = {640, 480};
     struct shiet_renderer_interface_s renderer = shiet_create_renderer_interface("OpenGL");
 
-    struct shiet_polygon_texture_s texture;
     struct shiet_polygon_triangle_stack_s *triangles = shiet_tristack_create(1);
     struct shiet_polygon_triangle_stack_s *transformedTriangles = shiet_tristack_create(triangles->capacity);
-
-    time_t timer = time(NULL); /* Used to toggle between linear and nearest texture filtering.*/
 
     /* Initialize the renderer.*/
     {
@@ -44,49 +40,7 @@ int main(void)
         trirot_initialize_screen_geometry(renderResolution.width, renderResolution.height);
     }
 
-    /* Set up a basic texture.*/
-    {
-        texture.width = 2;
-        texture.height = 2;
-        texture.filtering = SHIET_TEXTURE_FILTER_LINEAR;
-        texture.pixelArray = malloc(texture.width * texture.height * 4);
-        texture.pixelArray16bit = malloc(texture.width * texture.height * sizeof(texture.pixelArray16bit[0]));
-
-        /* TODO: Set the texture's wrapping mode to clamp-to-edge.*/
-
-        /* Top left.*/
-        texture.pixelArray[0] = 255;
-        texture.pixelArray[1] = 0;
-        texture.pixelArray[2] = 0;
-        texture.pixelArray[3] = 255;
-
-        /* Top right.*/
-        texture.pixelArray[4] = 0;
-        texture.pixelArray[5] = 255;
-        texture.pixelArray[6] = 0;
-        texture.pixelArray[7] = 255;
-
-        /* Bottom left.*/
-        texture.pixelArray[8] = 255;
-        texture.pixelArray[9] = 0;
-        texture.pixelArray[10] = 255;
-        texture.pixelArray[11] = 255;
-
-        /* Bottom right.*/
-        texture.pixelArray[12] = 0;
-        texture.pixelArray[13] = 0;
-        texture.pixelArray[14] = 255;
-        texture.pixelArray[15] = 255;
-
-        texture.pixelArray16bit[0] = 0xf801; /* Top left:  255,0,0.  */
-        texture.pixelArray16bit[1] = 0x7c1;  /* Top right: 0,255,0.  */
-        texture.pixelArray16bit[2] = 0xf83f; /* Top right: 255,0,255.*/
-        texture.pixelArray16bit[3] = 0x3f;   /* Top right: 0,0,255.  */
-
-        renderer.rasterizer.upload_texture(&texture);
-    }
-
-    /* Set up a triangle in the middle of the screen.*/
+    /* Create the triangle that will be rendered in the middle of the screen.*/
     {
         struct shiet_polygon_triangle_s triangle;
 
@@ -111,7 +65,7 @@ int main(void)
         triangle.vertex[2].u = 1;
         triangle.vertex[2].v = 1;
         
-        triangle.material.texture = &texture;
+        triangle.material.texture = NULL;
         triangle.material.baseColor[0] = 128;
         triangle.material.baseColor[1] = 192;
         triangle.material.baseColor[2] = 64;
@@ -129,16 +83,6 @@ int main(void)
                                               0, 0, 1.5,
                                               0, 0.01, 0);
 
-        if ((time(NULL) - timer) > 3)
-        {
-            texture.filtering = ((texture.filtering == SHIET_TEXTURE_FILTER_NEAREST)?
-                                 SHIET_TEXTURE_FILTER_LINEAR : SHIET_TEXTURE_FILTER_NEAREST);
-
-            renderer.rasterizer.update_texture(&texture);
-
-            timer = time(NULL);
-        }
-
         renderer.rasterizer.clear_frame();
         renderer.rasterizer.draw_triangles(transformedTriangles->data, transformedTriangles->count);
         renderer.window.update_window();
@@ -146,7 +90,6 @@ int main(void)
 
     shiet_tristack_free(triangles);
     shiet_tristack_free(transformedTriangles);
-    free(texture.pixelArray);
-    
+
     return 0;
 }

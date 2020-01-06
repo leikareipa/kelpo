883e81dd5c013f9513778a2ef96493caf8299783
leikareipa <drilledicecream@gmail.com>
2020-01-06 05:27:29 +0200

    rasterizer/glide: Use mipmapping
    Mipmapping appears to work on my real Voodoo card, but not in the
    PCem emulator. An issue with the code or with the emulator?

--- src/shiet_lib/renderer/rasterizer/glide3/rasterizer_glide3.c
+++ src/shiet_lib/renderer/rasterizer/glide3/rasterizer_glide3.c
@@ -28,7 +28,7 @@ static FxU32 CURRENT_TEXTURE_ADDRESS = 0;
 struct glide_vert_s
 {
     FxFloat x, y;
-    FxFloat w;
+    FxFloat q;
     FxFloat r, g, b;
     FxFloat s, t;
 };
@@ -49,17 +49,22 @@ void shiet_rasterizer_glide3__initialize(void)
     grDepthMask(FXTRUE);
 
     /* Texturing.*/
-    grTexClampMode(GR_TMU0,
-                   GR_TEXTURECLAMP_WRAP,
-                   GR_TEXTURECLAMP_WRAP);
-    grTexCombine(GR_TMU0,
-                 GR_COMBINE_FUNCTION_LOCAL,
-                 GR_COMBINE_FACTOR_NONE,
-                 GR_COMBINE_FUNCTION_LOCAL,
-                 GR_COMBINE_FACTOR_NONE,
-                 FXFALSE,
-                 FXFALSE); /* Set up for decal texturing.*/
-    grTexMipMapMode(GR_TMU0, GR_MIPMAP_DISABLE, FXFALSE);
+    {
+        grTexClampMode(GR_TMU0,
+                       GR_TEXTURECLAMP_WRAP,
+                       GR_TEXTURECLAMP_WRAP);
+
+        /* Set up for decal texturing.*/
+        grTexCombine(GR_TMU0,
+                     GR_COMBINE_FUNCTION_LOCAL,
+                     GR_COMBINE_FACTOR_NONE,
+                     GR_COMBINE_FUNCTION_LOCAL,
+                     GR_COMBINE_FACTOR_NONE,
+                     FXFALSE,
+                     FXFALSE);
+
+        grTexLodBiasValue(GR_TMU0, 0.5);
+    }
 
     CURRENT_TEXTURE_ADDRESS = grTexMinAddress(GR_TMU0);
 
@@ -73,9 +78,26 @@ void shiet_rasterizer_glide3__clear_frame(void)
     return;
 }
 
+static GrLOD_t lod_for_size(const unsigned size)
+{
+    switch (size)
+    {
+        case 1: return GR_LOD_LOG2_1;
+        case 2: return GR_LOD_LOG2_2;
+        case 4: return GR_LOD_LOG2_4;
+        case 8: return GR_LOD_LOG2_8;
+        case 16: return GR_LOD_LOG2_16;
+        case 32: return GR_LOD_LOG2_32;
+        case 64: return GR_LOD_LOG2_64;
+        case 128: return GR_LOD_LOG2_128;
+        case 256: return GR_LOD_LOG2_256;
+        default: assert(0 && "Unknown size for calculating a LOD value for.");
+    }
+}
+
 static GrTexInfo generate_glide_texture_info(const struct shiet_polygon_texture_s *const texture)
 {
-    GrTexInfo info;
+    GrTexInfo info = {0};
 
     assert(texture &&
            "Glide 3.x renderer: Received a NULL texture.");
@@ -87,30 +109,29 @@ static GrTexInfo generate_glide_texture_info(const struct shiet_polygon_texture_
            (texture->width <= MAX_TEXTURE_SIZE) &&
            "Glide 3.x renderer: The given texture's dimensions are out of range.");
 
-    switch (texture->width)
+    if (texture->numMipLevels > 1)
     {
-        case 1: info.smallLodLog2 = info.largeLodLog2 = GR_LOD_LOG2_1; break;
-        case 2: info.smallLodLog2 = info.largeLodLog2 = GR_LOD_LOG2_2; break;
-        case 4: info.smallLodLog2 = info.largeLodLog2 = GR_LOD_LOG2_4; break;
-        case 8: info.smallLodLog2 = info.largeLodLog2 = GR_LOD_LOG2_8; break;
-        case 16: info.smallLodLog2 = info.largeLodLog2 = GR_LOD_LOG2_16; break;
-        case 32: info.smallLodLog2 = info.largeLodLog2 = GR_LOD_LOG2_32; break;
-        case 64: info.smallLodLog2 = info.largeLodLog2 = GR_LOD_LOG2_64; break;
-        case 128: info.smallLodLog2 = info.largeLodLog2 = GR_LOD_LOG2_128; break;
-        case 256: info.smallLodLog2 = info.largeLodLog2 = GR_LOD_LOG2_256; break;
+        info.smallLodLog2 = GR_LOD_LOG2_1;
+        info.largeLodLog2 = lod_for_size(texture->width);
+        info.data = NULL;
+    }
+    else
+    {
+        info.smallLodLog2 = info.largeLodLog2 = lod_for_size(texture->width);
+        info.data = texture->mipLevel[0];
     }
 
     info.aspectRatioLog2 = GR_ASPECT_LOG2_1x1;
     info.format = GR_TEXFMT_ARGB_1555;
-    info.data = (FxU16*)texture->mipLevel[0];
 
     return info;
 }
 
 void shiet_rasterizer_glide3__upload_texture(struct shiet_polygon_texture_s *const texture)
 {
-    GrTexInfo texInfo;
-    FxU32 textureSize; 
+    uint32_t m = 0;
+    FxU32 textureSize = 0;
+    GrTexInfo texInfo = {0};
 
     if (!texture)
     {
@@ -118,13 +139,31 @@ void shiet_rasterizer_glide3__upload_texture(struct shiet_polygon_texture_s *con
     }
 
     texInfo = generate_glide_texture_info(texture);
-    textureSize = grTexTextureMemRequired(GR_MIPMAPLEVELMASK_BOTH, &texInfo); 
+    textureSize = grTexTextureMemRequired(GR_MIPMAPLEVELMASK_BOTH, &texInfo);
 
     assert(((CURRENT_TEXTURE_ADDRESS + textureSize) <= grTexMaxAddress(GR_TMU0)) &&
            "Glide 3.x renderer: Not enough texture memory to store the given texture.");
 
-    grTexDownloadMipMap(GR_TMU0, CURRENT_TEXTURE_ADDRESS, GR_MIPMAPLEVELMASK_BOTH, &texInfo);
     texture->apiId = CURRENT_TEXTURE_ADDRESS;
+
+    if (texture->numMipLevels > 1)
+    {
+        for (m = 0; m < texture->numMipLevels; m++)
+        {
+            grTexDownloadMipMapLevel(GR_TMU0,
+                                     CURRENT_TEXTURE_ADDRESS,
+                                     lod_for_size(texture->width / pow(2, m)),
+                                     lod_for_size(texture->width),
+                                     GR_ASPECT_LOG2_1x1,
+                                     GR_TEXFMT_ARGB_1555,
+                                     GR_MIPMAPLEVELMASK_BOTH,
+                                     texture->mipLevel[m]);
+        }
+    }
+    else
+    {
+        grTexDownloadMipMap(GR_TMU0, CURRENT_TEXTURE_ADDRESS, GR_MIPMAPLEVELMASK_BOTH, &texInfo);
+    }
 
     CURRENT_TEXTURE_ADDRESS += textureSize;
 
@@ -166,8 +205,11 @@ void shiet_rasterizer_glide3__draw_triangles(const struct shiet_polygon_triangle
 
             glideVertex[v].x = triangles[i].vertex[v].x;
             glideVertex[v].y = triangles[i].vertex[v].y;
-			glideVertex[v].w = (1 / triangles[i].vertex[v].w);
+            glideVertex[v].q = (1 / triangles[i].vertex[v].w);
         }
+
+        /* TODO: Reduce state-switching (e.g. grTexFilterMode()) - batch the
+         * polygons by material etc.*/
     
         /* Set the rendering mode based on whether the triangle has a texture
          * or is solid-filled.*/
@@ -201,6 +243,10 @@ void shiet_rasterizer_glide3__draw_triangles(const struct shiet_polygon_triangle
                                 GR_TEXTUREFILTER_POINT_SAMPLED,
                                 GR_TEXTUREFILTER_POINT_SAMPLED);
             }
+
+            grTexMipMapMode(GR_TMU0,
+                            ((texture->numMipLevels > 1)? GR_MIPMAP_NEAREST : GR_MIPMAP_DISABLE),
+                            FXTRUE);
 
             grTexSource(GR_TMU0, texture->apiId, GR_MIPMAPLEVELMASK_BOTH, &texInfo);
 

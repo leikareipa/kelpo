697a45f76d60b86ee284be693fcc1104c0f1927c
leikareipa <drilledicecream@gmail.com>
2020-01-07 05:22:03 +0200

    examples: Add build scripts for Visual C++ 6.0

--- examples/rotating_cube_model/build_win32_mingw441.sh
+++ examples/rotating_cube_model/build_win32_mingw441.sh
@@ -1,17 +1,19 @@
 # Build a shiet render example for Win32 using TDM-GCC (MinGW) 4.4.1 via Wine in Linux.
 # Note that this will place the executable under shiet's root bin/ directory.
 
+MINGW441_PATH=~/compi/mingw441
+
+OUTPUT_FILE="../../bin/rotating_cube_model.exe"
+
 SRC_FILES="
 src/main.c
 ../common_src/transform_and_rotate_triangles.c
+../common_src/load_kac_1_0_mesh.c
 ../common_src/parse_command_line.c
-../../src/shiet_interface/interface.c
+../common_src/kac/import_kac_1_0.c
 ../../src/shiet_interface/polygon/triangle/triangle_stack.c
+../../src/shiet_interface/interface.c
 "
-
-OUTPUT_FILE="../../bin/rotating_triangle.exe"
-
-MINGW_PATH=~/compi/mingw441
 
 BUILD_OPTIONS="
 -nostdinc
@@ -21,8 +23,8 @@ BUILD_OPTIONS="
 -Wall
 -march=pentium
 -I../../src/
--isystem $MINGW_PATH/lib/gcc/mingw32/4.4.1/include
--isystem $MINGW_PATH/include
+-isystem $MINGW441_PATH/lib/gcc/mingw32/4.4.1/include
+-isystem $MINGW441_PATH/include
 "
 
-wine "$MINGW_PATH/bin/gcc.exe" $BUILD_OPTIONS -o $OUTPUT_FILE $SRC_FILES -lm
+wine "$MINGW441_PATH/bin/gcc.exe" $BUILD_OPTIONS -o $OUTPUT_FILE $SRC_FILES -lm
---
+++ examples/rotating_triangle/build_win32_vc6.sh
@@ -0,0 +1,25 @@
+# Build a shiet render example for Win32 using Microsoft Visual C++ 6 via Wine in Linux.
+# Note that this will place the executable under shiet's root bin/ directory.
+
+VC6_PATH=~/compi/vc60
+
+OUTPUT_FILE="../../bin/rotating_triangle.exe"
+
+SRC_FILES="
+src/main.c
+../common_src/transform_and_rotate_triangles.c
+../common_src/parse_command_line.c
+../../src/shiet_interface/interface.c
+../../src/shiet_interface/polygon/triangle/triangle_stack.c
+"
+
+BUILD_OPTIONS="
+/O2
+/G4
+/Fe$OUTPUT_FILE
+/Fo./bin/
+/I../../src/
+/I$VC6_PATH/INCLUDE
+"
+
+wine "$VC6_PATH/BIN/cl.exe" $BUILD_OPTIONS $SRC_FILES "/link /LIBPATH:$VC6_PATH/LIB"

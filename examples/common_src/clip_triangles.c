8a07030c2f97d37865e38533944a3f35f3cc3c86
leikareipa <drilledicecream@gmail.com>
2020-08-22 00:52:19 +0300

    examples/common: Add a triangle clipper

---
+++ examples/common_src/clip_triangles.c
@@ -0,0 +1,164 @@
+/*
+ * 2020 Tarpeeksi Hyvae Soft
+ * 
+ * A C adaptation of Benny Bobaganoosh's triangle clipper.
+ * 
+ * Full attribution for Benny's original code:
+ * {
+ *     Copyright (c) 2014, Benny Bobaganoosh, https://github.com/BennyQBD/3DSoftwareRenderer
+ *     All rights reserved.
+ * 
+ *     Redistribution and use in source and binary forms, with or without
+ *     modification, are permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this
+ *        list of conditions and the following disclaimer.
+ *     2. Redistributions in binary form must reproduce the above copyright notice,
+ *        this list of conditions and the following disclaimer in the documentation
+ *        and/or other materials provided with the distribution.
+ *
+ *     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *     ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *     WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *     DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ *     ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *     (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *     LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *     ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *     (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * }
+ * 
+ */
+
+#include <assert.h>
+#include <kelpo_interface/polygon/triangle/triangle.h>
+#include <kelpo_interface/polygon/vertex.h>
+#include <kelpo_interface/common/globals.h>
+#include "clip_triangles.h"
+
+/* A buffer to store the triangles we've clipped from a given input triangle.
+ * An pointer to this buffer will be returned to the caller when they request
+ * to have a triangle clipped.*/
+static struct kelpo_polygon_triangle_s CLIPPED_TRIANGLES[TRICLIP_MAX_NUM_CLIPPED_TRIANGLES];
+
+/* Temporary buffers to hold vertex data during clipping.*/
+static struct vert_buffer_s
+{
+    struct kelpo_polygon_vertex_s v[TRICLIP_MAX_NUM_CLIPPED_VERTICES];
+    unsigned idx;
+} VERTEX_BUFFER_1,
+  VERTEX_BUFFER_2;
+
+static float vertex_component(const struct kelpo_polygon_vertex_s *const vertex,
+                              const unsigned componentIdx)
+{
+    switch (componentIdx)
+    {
+        case 0: return vertex->x;
+        case 1: return vertex->y;
+        case 2: return vertex->z;
+        default: assert(0 && "Unknown vertex component index.");
+    }
+}
+
+static void clip_polygon_component(struct vert_buffer_s *vertsIn,
+                                   struct vert_buffer_s *vertsOut,
+                                   const unsigned componentIdx,
+                                   const float componentFactor)
+{
+    unsigned i = 0;
+    struct kelpo_polygon_vertex_s *prevVert = &vertsIn->v[vertsIn->idx - 1];
+    float prevComponent = (vertex_component(prevVert, componentIdx) * componentFactor);
+    int prevInside = (prevComponent <= prevVert->w);
+
+    while (i < vertsIn->idx)
+    {
+        struct kelpo_polygon_vertex_s *curVert = &vertsIn->v[i++];
+
+        float curComponent = (vertex_component(curVert, componentIdx) * componentFactor);
+        int curInside = (curComponent <= curVert->w);
+
+        if (curInside ^ prevInside)
+        {
+            float lerpStep = (prevVert->w - prevComponent) / ((prevVert->w - prevComponent) - (curVert->w - curComponent));
+
+            vertsOut->v[vertsOut->idx].x = KELPO_LERP(prevVert->x, curVert->x, lerpStep);
+            vertsOut->v[vertsOut->idx].y = KELPO_LERP(prevVert->y, curVert->y, lerpStep);
+            vertsOut->v[vertsOut->idx].z = KELPO_LERP(prevVert->z, curVert->z, lerpStep);
+            vertsOut->v[vertsOut->idx].w = KELPO_LERP(prevVert->w, curVert->w, lerpStep);
+
+            vertsOut->v[vertsOut->idx].u = KELPO_LERP(prevVert->u, curVert->u, lerpStep);
+            vertsOut->v[vertsOut->idx].v = KELPO_LERP(prevVert->v, curVert->v, lerpStep);
+
+            vertsOut->idx++;
+        }
+
+        if (curInside)
+        {
+            vertsOut->v[vertsOut->idx++] = *curVert;
+        }
+
+        prevVert = curVert;
+        prevComponent = curComponent;
+        prevInside = curInside;
+    }
+
+    return;
+}
+
+static int clip_polygon_axis(const int componentIdx)
+{
+    clip_polygon_component(&VERTEX_BUFFER_1, &VERTEX_BUFFER_2, componentIdx, 1);
+    VERTEX_BUFFER_1.idx = 0;
+    
+    if (VERTEX_BUFFER_2.idx == 0)
+    {
+        return 0;
+    }
+
+    clip_polygon_component(&VERTEX_BUFFER_2, &VERTEX_BUFFER_1, componentIdx, -1);
+    VERTEX_BUFFER_2.idx = 0;
+
+    return (VERTEX_BUFFER_1.idx != 0);
+}
+
+unsigned triclip_clip_triangle(const struct kelpo_polygon_triangle_s *const triangle,
+                               struct kelpo_polygon_triangle_s **dstClippedTriangles)
+{
+    unsigned numClippedTriangles = 0;
+    
+    *dstClippedTriangles = CLIPPED_TRIANGLES;
+
+    VERTEX_BUFFER_1.idx = 0;
+    VERTEX_BUFFER_2.idx = 0;
+
+    VERTEX_BUFFER_1.v[VERTEX_BUFFER_1.idx++] = triangle->vertex[0];
+    VERTEX_BUFFER_1.v[VERTEX_BUFFER_1.idx++] = triangle->vertex[1];
+    VERTEX_BUFFER_1.v[VERTEX_BUFFER_1.idx++] = triangle->vertex[2];
+
+    if (clip_polygon_axis(0) &&
+        clip_polygon_axis(1) &&
+        clip_polygon_axis(2))
+    {
+        unsigned i = 0;
+        struct kelpo_polygon_vertex_s *initialVertex = &VERTEX_BUFFER_1.v[0];
+
+        for (i = 1; i < (VERTEX_BUFFER_1.idx - 1); i++)
+        {
+            CLIPPED_TRIANGLES[numClippedTriangles].texture = triangle->texture;
+            CLIPPED_TRIANGLES[numClippedTriangles].flags = triangle->flags;
+            
+            CLIPPED_TRIANGLES[numClippedTriangles].vertex[0] = *initialVertex;
+            CLIPPED_TRIANGLES[numClippedTriangles].vertex[1] = VERTEX_BUFFER_1.v[i];
+            CLIPPED_TRIANGLES[numClippedTriangles].vertex[2] = VERTEX_BUFFER_1.v[i+1];
+
+            if (++numClippedTriangles >= TRICLIP_MAX_NUM_CLIPPED_TRIANGLES)
+            {
+                break;
+            }
+        }
+    }
+    
+    return numClippedTriangles;
+}
---
+++ examples/common_src/clip_triangles.h
@@ -0,0 +1,29 @@
+/*
+ * 2020 Tarpeeksi Hyvae Soft
+ * 
+ * A C adaptation of Benny Bobaganoosh's triangle clipper (https://github.com/BennyQBD/3DSoftwareRenderer).
+ * 
+ */
+
+#ifndef KELPO_EXAMPLES_COMMON_CLIP_TRIANGLES_H
+#define KELPO_EXAMPLES_COMMON_CLIP_TRIANGLES_H
+
+/* The maximum number of triangles and vertices we can clip a source triangle into.
+ * These will be used to size the working buffers on the stack, so better conservative
+ * than greedy values.*/
+#define TRICLIP_MAX_NUM_CLIPPED_VERTICES 9
+#define TRICLIP_MAX_NUM_CLIPPED_TRIANGLES (TRICLIP_MAX_NUM_CLIPPED_VERTICES - 2)
+
+struct kelpo_polygon_triangle_s;
+
+/* Clips the given triangle (whose vertices are in [-1,1] clip space) against the
+ * view frustum, The 'dstClippedTriangles' pointer will be assigned the address of a
+ * memory buffer holding the clipped triangles - the original triangle will not be
+ * modified. The clipping buffer to which a pointer is given is shared between all
+ * calls to this function, so its contents can be trusted to remain valid only until
+ * the function is called again. Returns the number of triangles held in the memory
+ * buffer at the time the function exited; or 0 if the triangle could not be clipped.*/
+unsigned triclip_clip_triangle(const struct kelpo_polygon_triangle_s *const triangle,
+                               struct kelpo_polygon_triangle_s **dstClippedTriangles);
+
+#endif

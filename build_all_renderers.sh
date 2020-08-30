c782f14dd4ab340ef77ba812522b49a60654e6ff
leikareipa <drilledicecream@gmail.com>
2020-08-30 04:14:41 +0300

    Add a script to build all renderers

---
+++ build_all_renderers.sh
@@ -0,0 +1,7 @@
+echo "NOTE: Build warnings are suppressed."
+
+for buildScript in build_renderer_*.sh
+do
+    echo "Executing script $buildScript..."
+    ./"$buildScript" 2>&1 | grep "error"
+done

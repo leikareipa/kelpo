e4e63b5bdaee8b030fbce5fbacbaba2b7282affe
leikareipa <drilledicecream@gmail.com>
2020-08-30 18:53:54 +0300

    rasterizer/glide-3: Implement batch rendering

--- src/kelpo_renderer/rasterizer/glide_3/rasterizer_glide_3.c
+++ src/kelpo_renderer/rasterizer/glide_3/rasterizer_glide_3.c
@@ -22,6 +22,10 @@
  * no texture metadata, only the texture memory pointer.*/
 static struct kelpoa_generic_stack_s *UPLOADED_TEXTURES;
 
+/* For temporary storage of vertices during rendering. Stack elements will be
+ * of type struct glide3_vertex_s.*/
+static struct kelpoa_generic_stack_s *GR3_VERTEX_CACHE;
+
 /* Minimum and maximum side length for any given texture.*/
 static const unsigned MAX_TEXTURE_SIZE = 256;
 static const unsigned MIN_TEXTURE_SIZE = 2;
@@ -32,7 +36,7 @@ static FxU32 CURRENT_TEXTURE_ADDRESS = 0;
 
 /* NOTE: This struct would ideally not be padded further by the compiler,
  * since we manually pass its byte-level structure to grVertexLayout().*/
-struct glide_vertex_s
+struct glide3_vertex_s
 {
     float x, y, q;
     float u, v;
@@ -41,9 +45,12 @@ struct glide_vertex_s
 
 void kelpo_rasterizer_glide_3__initialize(void)
 {
+    UPLOADED_TEXTURES = kelpoa_generic_stack__create(10, sizeof(FxU32));
+    GR3_VERTEX_CACHE = kelpoa_generic_stack__create(1000, sizeof(struct glide3_vertex_s));
+
     grColorMask(FXTRUE, FXFALSE);
 
-    /* Vertex data layout matches that in the glide_vertex_s struct.*/
+    /* Vertex data layout matches that in the glide3_vertex_s struct.*/
     grVertexLayout(GR_PARAM_XY, 0, GR_PARAM_ENABLE);
     grVertexLayout(GR_PARAM_Q, 8, GR_PARAM_ENABLE);
     grVertexLayout(GR_PARAM_ST0, 12, GR_PARAM_ENABLE);
@@ -70,9 +77,7 @@ void kelpo_rasterizer_glide_3__initialize(void)
                  FXFALSE);
     grTexLodBiasValue(GR_TMU0, 0.5);
 
-    CURRENT_TEXTURE_ADDRESS = grTexMinAddress(GR_TMU0);
-
-    UPLOADED_TEXTURES = kelpoa_generic_stack__create(10, sizeof(FxU32));
+    CURRENT_TEXTURE_ADDRESS = grTexMinAddress(GR_TMU0);  
 
     return;
 }
@@ -80,6 +85,7 @@ void kelpo_rasterizer_glide_3__initialize(void)
 void kelpo_rasterizer_glide_3__release(void)
 {
     kelpoa_generic_stack__free(UPLOADED_TEXTURES);
+    kelpoa_generic_stack__free(GR3_VERTEX_CACHE);
 
     return;
 }
@@ -221,68 +227,113 @@ void kelpo_rasterizer_glide_3__purge_textures(void)
 void kelpo_rasterizer_glide_3__draw_triangles(struct kelpo_polygon_triangle_s *const triangles,
                                               const unsigned numTriangles)
 {
-    unsigned i = 0, v = 0;
+    unsigned numTrianglesProcessed = 0;
+    unsigned numTrianglesInBatch = 0;
+    const struct kelpo_polygon_triangle_s *triangle = triangles;
 
-    for (i = 0; i < numTriangles; i++)
+    if ((3 * numTriangles) > GR3_VERTEX_CACHE->capacity)
     {
-        struct glide_vertex_s verts[3];
+        kelpoa_generic_stack__grow(GR3_VERTEX_CACHE, (3 * numTriangles));
+    }
 
-        for (v = 0; v < 3; v++)
+    /* Render the triangles in batches. Each batch consists of consecutive
+     * triangles that share a texture.*/
+    while (1)
+    {
+        unsigned v = 0;
+        struct glide3_vertex_s *const vertexCache = (struct glide3_vertex_s*)GR3_VERTEX_CACHE->data;
+        
+        /* Add the current triangle into the batch.*/
         {
-            verts[v].x = triangles[i].vertex[v].x;
-            verts[v].y = triangles[i].vertex[v].y;
-            verts[v].q = triangles[i].vertex[v].w;
-
-            ((char*)&verts[v].rgba)[0] = triangles[i].vertex[v].b;
-            ((char*)&verts[v].rgba)[1] = triangles[i].vertex[v].g;
-            ((char*)&verts[v].rgba)[2] = triangles[i].vertex[v].r;
-            ((char*)&verts[v].rgba)[3] = triangles[i].vertex[v].a;
-        }
-
-        if (!triangles[i].texture)
-        {
-            grColorCombine(GR_COMBINE_FUNCTION_LOCAL,
-                           GR_COMBINE_FACTOR_NONE,
-                           GR_COMBINE_LOCAL_ITERATED,
-                           GR_COMBINE_OTHER_NONE,
-                           FXFALSE);
-        }
-        else
-        {
-            /* TODO: Reduce state-switching - batch the polygons by material etc.*/
-
-            GrTexInfo texInfo = generate_glide_texture_info(triangles[i].texture);
-
             for (v = 0; v < 3; v++)
             {
-                verts[v].u = ((triangles[i].vertex[v].u * triangles[i].vertex[v].w) * 256);
-                verts[v].v = ((triangles[i].vertex[v].v * triangles[i].vertex[v].w) * 256);
+                const struct kelpo_polygon_vertex_s *const srcVertex = &triangle->vertex[v];
+                struct glide3_vertex_s *const dstVertex = &vertexCache[(numTrianglesInBatch * 3) + v];
+
+                dstVertex->x = srcVertex->x;
+                dstVertex->y = srcVertex->y;
+                dstVertex->q = srcVertex->w;
+
+                dstVertex->u = ((srcVertex->u * srcVertex->w) * 256);
+                dstVertex->v = ((srcVertex->v * srcVertex->w) * 256);
+
+                ((char*)&dstVertex->rgba)[0] = srcVertex->b;
+                ((char*)&dstVertex->rgba)[1] = srcVertex->g;
+                ((char*)&dstVertex->rgba)[2] = srcVertex->r;
+                ((char*)&dstVertex->rgba)[3] = srcVertex->a;
             }
 
-            grTexFilterMode(GR_TMU0,
-                            (triangles[i].texture->flags.noFiltering? GR_TEXTUREFILTER_POINT_SAMPLED : GR_TEXTUREFILTER_BILINEAR),
-                            (triangles[i].texture->flags.noFiltering? GR_TEXTUREFILTER_POINT_SAMPLED : GR_TEXTUREFILTER_BILINEAR));
-
-            grTexClampMode(GR_TMU0,
-                           (triangles[i].texture->flags.clamped? GR_TEXTURECLAMP_CLAMP : GR_TEXTURECLAMP_WRAP),
-                           (triangles[i].texture->flags.clamped? GR_TEXTURECLAMP_CLAMP : GR_TEXTURECLAMP_WRAP));
-
-            grTexMipMapMode(GR_TMU0,
-                            ((triangles[i].texture->numMipLevels > 1)? GR_MIPMAP_NEAREST : GR_MIPMAP_DISABLE),
-                            FXTRUE);
-
-            grTexSource(GR_TMU0, triangles[i].texture->apiId, GR_MIPMAPLEVELMASK_BOTH, &texInfo);
-
-            grColorCombine(GR_COMBINE_FUNCTION_SCALE_OTHER,
-                           GR_COMBINE_FACTOR_LOCAL,
-                           GR_COMBINE_LOCAL_ITERATED,
-                           GR_COMBINE_OTHER_TEXTURE,
-                           FXFALSE);
+            numTrianglesInBatch++;
+            numTrianglesProcessed++;
         }
 
-        grDrawTriangle(&verts[0],
-                       &verts[1],
-                       &verts[2]);
+        /* If we're at the end of the current batch, render its triangles and
+         * start a new batch.*/
+        {
+            const int isEndOfTriangles = (numTrianglesProcessed >= numTriangles);
+
+            const int hasTexture = (triangle->texture != NULL);
+            const uint32_t currentApiId = (hasTexture? triangle->texture->apiId : 0);
+            
+            const struct kelpo_polygon_triangle_s *nextTriangle = (isEndOfTriangles? NULL : (triangle + 1));
+            const int nextHasTexture = (nextTriangle? (nextTriangle->texture != NULL) : 0);
+            const uint32_t nextApiId = (nextHasTexture? nextTriangle->texture->apiId : 0);
+            const int isEndOfBatch = (isEndOfTriangles || (nextApiId != currentApiId));
+
+            if (isEndOfBatch)
+            {
+                if (!hasTexture)
+                {
+                    grColorCombine(GR_COMBINE_FUNCTION_LOCAL,
+                                   GR_COMBINE_FACTOR_NONE,
+                                   GR_COMBINE_LOCAL_ITERATED,
+                                   GR_COMBINE_OTHER_NONE,
+                                   FXFALSE);
+                }
+                else
+                {
+                    GrTexInfo texInfo = generate_glide_texture_info(triangle->texture);
+
+                    grTexFilterMode(GR_TMU0,
+                                    (triangle->texture->flags.noFiltering? GR_TEXTUREFILTER_POINT_SAMPLED : GR_TEXTUREFILTER_BILINEAR),
+                                    (triangle->texture->flags.noFiltering? GR_TEXTUREFILTER_POINT_SAMPLED : GR_TEXTUREFILTER_BILINEAR));
+
+                    grTexClampMode(GR_TMU0,
+                                   (triangle->texture->flags.clamped? GR_TEXTURECLAMP_CLAMP : GR_TEXTURECLAMP_WRAP),
+                                   (triangle->texture->flags.clamped? GR_TEXTURECLAMP_CLAMP : GR_TEXTURECLAMP_WRAP));
+
+                    grTexMipMapMode(GR_TMU0,
+                                    ((triangle->texture->numMipLevels > 1)? GR_MIPMAP_NEAREST : GR_MIPMAP_DISABLE),
+                                    FXTRUE);
+
+                    grTexSource(GR_TMU0, triangle->texture->apiId, GR_MIPMAPLEVELMASK_BOTH, &texInfo);
+
+                    grColorCombine(GR_COMBINE_FUNCTION_SCALE_OTHER,
+                                   GR_COMBINE_FACTOR_LOCAL,
+                                   GR_COMBINE_LOCAL_ITERATED,
+                                   GR_COMBINE_OTHER_TEXTURE,
+                                   FXFALSE);
+                }
+
+                for (v = 0; v < numTrianglesInBatch; v++)
+                {
+                    const unsigned vertexIdx = (v * 3);
+
+                    grDrawTriangle(&vertexCache[vertexIdx+0],
+                                   &vertexCache[vertexIdx+1],
+                                   &vertexCache[vertexIdx+2]);
+                }
+
+                if (isEndOfTriangles)
+                {
+                    break;
+                }
+
+                numTrianglesInBatch = 0;
+            }
+        }
+
+        triangle++;
     }
 
     return;

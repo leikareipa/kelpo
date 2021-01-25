2718f665e8fbec8134666dd203033134a61f5c5d
leikareipa <drilledicecream@gmail.com>
2021-01-25 09:12:14 +0200

    error: Use specific instead of generic error codes

--- src/kelpo_interface/error.c
+++ src/kelpo_interface/error.c
@@ -17,41 +17,29 @@ int KELPO_ERROR_VERBOSE = 1;
 /* The most recent error code, as reported to kelpo_error(). Will evaluate
  * to true if one or more errors have been reported since its value was set
  * to KELPOERR_NO_ERROR, and false if not.*/
-static uint32_t KELPO_ERROR_CODE = KELPOERR_NO_ERROR;
-
-/* The most recent error string, as reported to kelpo_error(). Will be set
- * to NULL if no errors have been reported.*/
-static const char *KELPO_ERROR_STRING = NULL;
+static enum kelpo_error_code_e KELPO_ERROR_CODE = KELPOERR_NO_ERROR;
 
 void kelpo_error_reset(void)
 {
     KELPO_ERROR_CODE = KELPOERR_NO_ERROR;
-    KELPO_ERROR_STRING = NULL;
 
     return;
 }
 
-uint32_t kelpo_error_code(void)
+enum kelpo_error_code_e kelpo_error_code(void)
 {
     return KELPO_ERROR_CODE;
 }
 
-const char* kelpo_error_string(void)
-{
-    return KELPO_ERROR_STRING;
-}
-
-void kelpo_error(const uint32_t errorCode,
-                 const char *const errorString)
+void kelpo_error(const enum kelpo_error_code_e errorCode)
 {
     KELPO_ERROR_CODE = errorCode;
-    KELPO_ERROR_STRING = errorString;
 
     if (KELPO_ERROR_VERBOSE)
     {
-        fprintf(stderr, "KELPO ERROR (%s): %s\n",
-                        kelpo_active_renderer_name(),
-                        errorString);
+        /* TODO: Error string instead of error code.*/
+        fprintf(stderr, "KELPO ERROR (%s): %u\n",
+                        errorCode);
     }
 
     return;
--- src/kelpo_interface/error.h
+++ src/kelpo_interface/error.h
@@ -17,19 +17,13 @@ extern int KELPO_ERROR_VERBOSE;
 
 /* Returns the most recent error code, or KELPOERR_NO_ERROR if no error has
  * occurred since the error code was last reset (by calling kelpo_error_reset()).*/
-uint32_t kelpo_error_code(void);
+enum kelpo_error_code_e kelpo_error_code(void);
 
-/* Returns the most recent error code, or NULL if no error has occurred since
- * the error string was last reset.*/
-const char* kelpo_error_string(void);
-
-/* Reset the error code and error string. Immediately after calling this
- * function and until an error occurs, kelpo_error_code() will return
- * KELPOERR_NO_ERROR.*/
+/* Reset the error code. Immediately after calling this function and until an
+ * error occurs, kelpo_error_code() will return KELPOERR_NO_ERROR.*/
 void kelpo_error_reset(void);
 
 /* Used by Kelpo's internals to report runtime errors.*/
-void kelpo_error(const uint32_t errorCode,
-                 const char *const errorString);
+void kelpo_error(const enum kelpo_error_code_e errorCode);
 
 #endif

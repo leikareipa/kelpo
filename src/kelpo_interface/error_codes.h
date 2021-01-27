a678d2fd404f9b0769e33b8599a5582e37513866
leikareipa <drilledicecream@gmail.com>
2021-01-27 06:00:10 +0200

    interface: Add some more error checking

--- src/kelpo_interface/error_codes.h
+++ src/kelpo_interface/error_codes.h
@@ -16,7 +16,9 @@ enum kelpo_error_code_e
     KELPOERR_VSYNC_CONTROL_NOT_SUPPORTED,
     KELPOERR_DISPLAY_MODE_NOT_SUPPORTED,
     KELPOERR_OUT_OF_VIDEO_MEMORY,
-    KELPOERR_API_CALL_FAILED
+    KELPOERR_API_CALL_FAILED, /* A call to an API function, e.g. glVertex3f() (OpenGL), failed. In some cases, the relevant API error code will have been printed into stderr.*/
+    KELPOERR_RENDERER_NOT_AVAILABLE, /* The given Kelpo renderer, e.g. Glide, isn't available/supported on the system.*/
+    KELPOERR_RENDERER_NOT_COMPATIBLE_WITH_INTERFACE /* The renderer was compiled for a different version of the interface.*/
 };
 
 #endif
--- src/kelpo_interface/interface.c
+++ src/kelpo_interface/interface.c
@@ -13,17 +13,25 @@ typedef void *(*dll_import_fn_t)(struct kelpo_interface_s *const);
 static struct kelpo_interface_s ACTIVE_INTERFACE = {0};
 
 /* Initializes a Kelpo interface for the given renderer, and sets 'dst' to point
- * to that interface. Returns 1 on success; 0 otherwise.*/
+ * to that interface. Returns 1 on success; 0 on failure. If the call fails,
+ * the dst pointer won't be modified.*/
 int kelpo_create_interface(const struct kelpo_interface_s **dst,
                            const char *const rendererName)
 {
     const char *dllFilename = NULL;
     dll_import_fn_t get_kelpo_interface = NULL;
 
+    /* For debugging purposes, record the user-provided renderer name. If the
+     * interface is successfully created, this will be replaced with the actual
+     * renderer name.*/
+    KELPO_COPY_RENDERER_NAME(ACTIVE_INTERFACE.metadata.rendererName, rendererName);
+
     /* If we currently have an active interface that hasn't yet been released.*/
     if (ACTIVE_INTERFACE.dllHandle &&
         !kelpo_release_interface(&ACTIVE_INTERFACE))
     {
+        /* kelpo_release_interface() is expected to have called kelpo_error(), so
+         * we can just return.*/
         return 0;
     }
 
@@ -34,31 +42,55 @@ int kelpo_create_interface(const struct kelpo_interface_s **dst,
     else if (strcmp(rendererName, "direct3d_6") == 0) dllFilename = "kelpo_renderer_direct3d_6.dll";
     else if (strcmp(rendererName, "direct3d_7") == 0) dllFilename = "kelpo_renderer_direct3d_7.dll";
 
-    assert(dllFilename && "Unknown renderer name.");
-
-    ACTIVE_INTERFACE.dllHandle = LoadLibraryA(dllFilename);
-    assert(ACTIVE_INTERFACE.dllHandle && "Failed to load the renderer's DLL.");
+    if (!dllFilename ||
+        !(ACTIVE_INTERFACE.dllHandle = LoadLibraryA(dllFilename)))
+    {
+        kelpo_error(KELPOERR_RENDERER_NOT_AVAILABLE);
+        return 0;
+    }
 
     get_kelpo_interface = (dll_import_fn_t)GetProcAddress(ACTIVE_INTERFACE.dllHandle, "export_interface");
-    assert(get_kelpo_interface && "Failed to import the renderer from the DLL.");
+    assert(get_kelpo_interface && "Malformed renderer DLL; required export not found.");
 
-    get_kelpo_interface(&ACTIVE_INTERFACE);
-    assert((ACTIVE_INTERFACE.metadata.rendererVersionMajor == KELPO_INTERFACE_VERSION_MAJOR) &&
-           "The renderer's version is not compatible with this version of Kelpo.");
+    if (!get_kelpo_interface(&ACTIVE_INTERFACE))
+    {
+        /* The function call is expected to have called kelpo_error(), so we
+         * can just return.*/
+        return 0;
+    }
+
+    if (ACTIVE_INTERFACE.metadata.rendererVersionMajor != KELPO_INTERFACE_VERSION_MAJOR)
+    {
+        kelpo_error(KELPOERR_RENDERER_NOT_COMPATIBLE_WITH_INTERFACE);
+        return 0;
+    }
 
     *dst = &ACTIVE_INTERFACE;
     return 1;
 }
 
-/* Returns 1 on success; 0 otherwise.*/
+/* Deallocates the given interface. The interface can no longer be used after
+ * calling this function. Returns 1 on success; 0 on failure. If the call succeeds,
+ * the interface's function pointers and data will be reset to NULL values;
+ * otherwise, the values won't be modified.*/
 int kelpo_release_interface(const struct kelpo_interface_s *const kelpoInterface)
 {
+    /* The given interface pointer could be NULL e.g. if it was initialized that
+     * way and a call to kelpo_create_interface() failed. So we allow it and just
+     * silently return.*/
+    if (!kelpoInterface)
+    {
+        return 1;
+    }
+
     assert((kelpoInterface == &ACTIVE_INTERFACE) && 
            "Can't release an interface that isn't active.");
     
     if (!ACTIVE_INTERFACE.rasterizer.unload_textures() ||
         !ACTIVE_INTERFACE.window.release())
     {
+        /* The function calls are expected to have called kelpo_error(), so we
+         * can just return.*/
         return 0;
     }
 
